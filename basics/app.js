439057d8786acbe314d2135d92c465ac2e60dac7
HamzaAshraf160 <hamza.ashraf@cybersoph.com>
2025-09-11 01:55:23 +0500

    Data less component, x-init, Alpine.store Done

--- basics/app.js
+++ basics/app.js
@@ -5,4 +5,9 @@ document.addEventListener('alpine:init',  () => {
             this.open = !this.open
         }
     }))
+
+    Alpine.store('currentUser', {
+        username:'hamza555',
+        posts:['Post 1', 'Post 2', 'Post 3']
+    })
 })
